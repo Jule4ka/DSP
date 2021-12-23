c564e5e0d6d4ac7f2779e501eea9dcf2601b5f69
Jule4ka <jpjul13@gmail.com>
2021-12-23 16:05:20 +0100

    added navigation

--- app.py
+++ app.py
@@ -3,19 +3,30 @@ import pandas as pd
 from flask_navigation import Navigation
 
 app = Flask("__name__")
+nav = Navigation(app)
+
+# initializing navigations
+nav.Bar('top', [
+    nav.Item('Assets Overview', 'assets_overview'),
+    nav.Item('Asset Components', 'asset_components')
+])
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
+def navpage():
+    return render_template("navpage.html")
+
+
+@app.route('/assets_overview', methods=['GET', 'POST'])
 def assets_overview():
-    return render_template("start.html")
+    dataset = pd.read_excel("data/Gemeente Almere bruggen paspoort gegevens.xlsx")
+    return render_template('assets_overview.html', data=dataset.to_html())
+    # return render_template('assets_overview.html', data=dataset.to_dict())
 
 
-@app.route('/assets-overview', methods=['GET', 'POST'])
-def data():
-    if request.method == 'POST':
-        dataset = pd.read_excel("data/Gemeente Almere bruggen paspoort gegevens.xlsx")
-        return render_template('assets-overview.html', data=dataset.to_html())
-        #return render_template('assets-overview.html', data=dataset.to_dict())
+@app.route('/asset-components', methods=['GET', 'POST'])
+def asset_components():
+    return render_template("asset_components.html")
 
 
 if __name__ == '__main__':
