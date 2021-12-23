9115ccc14c74ca4571816772bc87fd72a88386c9
Jule4ka <jpjul13@gmail.com>
2021-12-23 15:13:04 +0100

    initial setup

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+import pandas as pd
+
+app = Flask("__name__")
+
+
+@app.route('/', methods=['GET', 'POST'])
+def assets_overview():
+    return render_template("start.html")
+
+
+@app.route('/assets-overview', methods=['GET', 'POST'])
+def data():
+    if request.method == 'POST':
+        dataset = pd.read_excel("data/Gemeente Almere bruggen paspoort gegevens.xlsx")
+        return render_template('assets-overview.html', data=dataset.to_html())
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
