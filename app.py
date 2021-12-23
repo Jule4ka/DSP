8ae778e0627396095cfe0656fe1cfbc9cdfc98c6
Jule4ka <jpjul13@gmail.com>
2021-12-23 16:36:26 +0100

    changed format

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_navigation import Navigation
 app = Flask("__name__")
 nav = Navigation(app)
 
-# initializing navigations
+# initializing navigation
 nav.Bar('top', [
     nav.Item('Assets Overview', 'assets_overview'),
     nav.Item('Asset Components', 'asset_components')
@@ -14,14 +14,14 @@ nav.Bar('top', [
 
 @app.route('/')
 def navpage():
-    return render_template("navpage.html", title = "Home Page")
+    return render_template("navpage.html", title="Home Page")
 
 
 @app.route('/assets_overview', methods=['GET', 'POST'])
 def assets_overview():
     dataset = pd.read_excel("data/Gemeente Almere bruggen paspoort gegevens.xlsx")
     return render_template('assets_overview.html', data=dataset.to_html())
-    # return render_template('assets_overview.html', data=dataset.to_dict())
+    # return render_template('assets_overview.html', data=dataset.to_dict()) #transform to dictionary
 
 
 @app.route('/asset-components', methods=['GET', 'POST'])
@@ -29,5 +29,6 @@ def asset_components():
     return render_template("asset_components.html")
 
 
+# run the application
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
