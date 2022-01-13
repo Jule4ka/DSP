a7ce654a61ba9d0215fd521f62f61b2a11eafc8c
DaanHorenberg <Paddenstoel6!>
2022-01-13 20:52:41 +0100

    change password

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app.config['MYSQL_HOST'] = 'localhost'
 # MySQL username
 app.config['MYSQL_USER'] = 'root'
 # MySQL password here in my case password is null so i left empty
-app.config['MYSQL_PASSWORD'] = 'DSPB1111'
+app.config['MYSQL_PASSWORD'] = 'DSPB1'
 # Database name In my case database name is projectreporting
 app.config['MYSQL_DB'] = 'dummy_db'
 
